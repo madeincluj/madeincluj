1171989cdc800a437dcc236714ebf5f3ff19c366
Dan Burzo <dburzo@sdl.com>
2013-10-18 16:46:16 +0300

    scraped building footprint from OSM

---
+++ js/gmaps.js
@@ -0,0 +1,35 @@
+var GMAPS_STYLES = [
+  {
+    "featureType": "all",
+    "elementType": "all",
+    "stylers": [
+      {
+        "hue": "#2DB5E2"
+      },
+      {
+        "saturation": "-20"
+      }
+    ]
+  },
+  {
+    "featureType": "all",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "lightness": "100"
+      },
+      {
+        "visibility": "simplified"
+      }
+    ]
+  }
+];
