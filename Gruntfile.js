bbce764e33738f82c7e8be59eb816aa100c5ad49
Adriana Saroz <sarozadriana@gmail.com>
2013-11-03 00:42:35 +0200

    Moved hardcoded pictures info to yaml.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,21 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		useminPrepare: {
 			html: 'index.html'
+		},
+
+		yaml: {
+			pictures: {
+				files: [{
+					expand: true,
+					cwd: 'metadata/',
+					src: ['*.yaml'],
+					dest: 'json/'
+				}]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-yaml');
+	grunt.registerTask('metadata', 'Compile picture metadata from YAML source', ['yaml:pictures']);
 };
