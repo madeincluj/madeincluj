cf62938d77b0bc3aab033dce76c3a262a656d9ea
Dan Burzo <danburzo@gmail.com>
2013-11-02 17:34:18 +0200

    Added JSON->HTML generator for buildings

---
+++ tools/dg.js
@@ -0,0 +1,29 @@
+var ejs = require('ejs');
+var slug = require('slug');
+var fs = require('fs-extra');
+
+slug.charmap['ș'] = 's';
+slug.charmap['ț'] = 't';
+slug.charmap['ă'] = 'a';
+slug.charmap['â'] = 'a';
+slug.charmap['î'] = 'i';
+
+var json_dir = '../collection/dg/json/metadata/';
+var output_dir = 'site/cladiri/';
+var template = 'templates/building.html';
+
+var jsons = fs.readdirSync(json_dir);
+var tmpl = fs.readFileSync(template, 'utf8');
+
+fs.removeSync(output_dir);
+
+jsons.forEach(function(filepath) {
+	var json = JSON.parse(fs.readFileSync(json_dir + filepath), 'utf8');
+	console.log(json);
+	var output = ejs.render(tmpl, {
+		building: json
+	});
+	var slug_path = slug(json.name || json.id.replace('way/', '')).toLowerCase();
+	var output_path = output_dir + slug_path + '/index.html';
+	fs.outputFile(output_path, output);
+});
