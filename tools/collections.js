d50edc8c09a8540da52d8fddd7a254a5ac4b2af0
Dan Burzo <dburzo@sdl.com>
2013-11-05 13:35:10 +0200

    Added website generation for collections

---
+++ tools/collections.js
@@ -0,0 +1,51 @@
+// collections website generator
+
+var ejs = require('ejs');
+var slug = require('slug');
+var fs = require('fs-extra');
+
+var collections = {
+	'fortepan' : {
+		json: '../collection/fortepan/json/fortepan.json',
+		img_base_url: 'http://madeincluj.s3-website-eu-west-1.amazonaws.com/collection/fortepan/',
+		original_path: 'original/',
+		large_path: 'large/',
+		thumb_path: 'thumb/'
+	},
+	'fotografii-istorice': {
+		json: '../collection/historical-collection/json/historical-collection.json',
+		img_base_url: 'http://madeincluj.s3-website-eu-west-1.amazonaws.com/collection/historical-collection/',
+		original_path: 'original/',
+		large_path: 'large/',
+		thumb_path: 'thumb/'
+	},
+	'arhiva' : {
+		json: '../collection/historical-photography/json/historical-photography.json',
+		img_base_url: 'http://madeincluj.s3-website-eu-west-1.amazonaws.com/collection/historical-photography/',
+		original_path: 'original/',
+		large_path: 'large/',
+		thumb_path: 'thumb/'
+	}
+};
+
+var output_dir = 'site/colectie/';
+var collection_tmpl = 'templates/collection.html';
+
+for (var slug in collections) {
+	var collection = collections[slug];
+	var json = JSON.parse(fs.readFileSync(collection.json, 'utf8'));
+	var tmpl = fs.readFileSync(collection_tmpl, 'utf8');
+
+	json.features.forEach(function(feature) {
+		feature.properties.photo = {
+			original: collection.img_base_url + collection.original_path + feature.properties.id + '.jpg',
+			large: collection.img_base_url + collection.large_path + feature.properties.id + '.jpg',
+			thumb: collection.img_base_url + collection.thumb_path + feature.properties.id + '.jpg'
+		};
+	});
+
+	var output = ejs.render(tmpl, {
+		collection: json
+	});
+	fs.outputFile(output_dir + slug + '/index.html', output);
+};
