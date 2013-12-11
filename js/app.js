b137c110e56e65fdef822a570b4f76882115f060
Adriana Saroz <sarozadriana@gmail.com>
2013-12-11 02:39:19 +0200

    Basic template and design.

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ var MIC = {
 
 	showPopup: function(content) {
 		this.popup.show().find('.popup-content').html(content);
+		MIC._initPopupGallery();
 		$(document).on('keyup', this.escHandler);
 	},
 
@@ -87,14 +88,81 @@ var MIC = {
 	},
 
 	_initPopups: function() {
+
+		moveToItem = function(from_item, to_item) {
+			var gallery_prev = that.popup.find('.gallery-prev');
+			var gallery_next = that.popup.find('.gallery-next');
+
+			if (from_item.hasClass('first')) {
+				gallery_prev.css('z-index', 100)
+
+			} else if (to_item.hasClass('first')) {
+				gallery_prev.css('z-index', 0)
+			}
+
+			if (from_item.hasClass('last')) {
+				gallery_next.css('z-index', 100)
+
+			} else if (to_item.hasClass('last')) {
+				gallery_next.css('z-index', 0)
+			}
+
+			from_item.removeClass('active');
+			to_item.addClass('active');
+
+			var position = to_item.position().left;
+			if (!to_item.hasClass('first')) {
+				position -= gallery_prev.width()
+			}
+
+			var gallery = that.popup.find('.gallery');
+			gallery.animate({left: -position});
+		}
+
 		var that = this;
 		var popup = this.popup = $("#popup-container");
+
 		this.popup.on('click', '.close-popup', function() {
 			that.hidePopup();
 			return false;
+
+		}).on('click', '.gallery-prev', function() {
+			var from = $(that.popup).find('.gallery-item.active');
+			var to = from.prev();
+			moveToItem(from, to);
+
+		}).on('click', '.gallery-next', function() {
+			var from = $(that.popup).find('.gallery-item.active');
+			var to = from.next();
+			moveToItem(from, to);
+
 		}).on('click', '.item-gallery-thumbs span', function() {
 			var src = $(this).data('large');
 			var img = popup.find('.item-gallery-view').css('background-image', 'url(' + src + ')');
 		});
+	},
+
+	_initPopupGallery: function() {
+		// Set max-width of gallery images.
+		var width = this.popup.width();
+		// Information area should be 30%
+		this.popup.find('.gallery-item.item-info').width(width * 0.3);
+		// Images should not expand more than 70%. Panoramas should not overflow this width.
+		this.popup.find('.gallery-item.item-image').css('maxWidth', width * 0.7);
+
+		// Initialize css helper classes.
+		this.popup.find('.gallery-item:first').addClass('first active');
+		this.popup.find('.gallery-item:last').addClass('last');
+
+		var gallery = this.popup.find('.gallery');
+		var item_images = gallery.find('.gallery-item.item-image:not(:has(img))')
+
+		item_images.each(function() {
+			var that = this;
+			$('<img>').load(function(e) {
+				gallery.width(gallery.width() + this.width)
+				$(this).appendTo(that);
+			}).attr('src', $(this).attr('data-src'));
+		})
 	}
 };
